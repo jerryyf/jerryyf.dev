6eed900d71d9dad627f481c126186403d3a1fdde
jerryyf <jerryyf@linsomnia.net>
2024-12-07 14:18:38 +1100

    chore: disallow gptbot

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,10 @@ export default function robots() {
       {
         userAgent: '*',
       },
+      {
+        userAgent: 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko); compatible; GPTBot/1.1; +https://openai.com/gptbot',
+        disallow: ['/']
+      }
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
   }
