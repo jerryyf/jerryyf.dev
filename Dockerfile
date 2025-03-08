8361cdb337fffb53ed1765b77440a385ad0c95b3
jerryyf <jerryyf@linsomnia.net>
2025-03-08 16:28:21 +1100

    ci: prepend NEXT_PUBLIC_ to env vars

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,10 @@ RUN pnpm install
 
 COPY . .
 
-RUN --mount=type=secret,id=aws_region,env=AWS_REGION \
+RUN --mount=type=secret,id=NEXT_PUBLIC_AWS_REGION,env=NEXT_PUBLIC_AWS_REGION \
+    --mount=type=secret,id=NEXT_PUBLIC_AWS_ACCESS_KEY_ID,env=NEXT_PUBLIC_AWS_ACCESS_KEY_ID \
+    --mount=type=secret,id=NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,env=NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY \
+    --mount=type=secret,id=NEXT_PUBLIC_LAMBDA_FUNCTION_NAME,env=NEXT_PUBLIC_LAMBDA_FUNCTION_NAME \
     pnpm build
 
 EXPOSE 3000
--- app/lib/lambda.ts
+++ app/lib/lambda.ts
@@ -3,13 +3,18 @@ import { fromEnv } from '@aws-sdk/credential-providers'
 
 export async function triggerLambda(visitorDetails:Object) {
 
+  // Workaround for Next.js environment variables during build time
+  process.env.AWS_ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID
+  process.env.AWS_SECRET_ACCESS_KEY = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
+  process.env.LAMBDA_FUNCTION_NAME = process.env.NEXT_PUBLIC_LAMBDA_FUNCTION_NAME
+
   const client = new LambdaClient({
     credentials: fromEnv(),
-    region: process.env['AWS_REGION']
+    region: process.env.NEXT_PUBLIC_AWS_REGION
   })
 
   const input = {
-    FunctionName: process.env['LAMBDA_FUNCTION_NAME'],
+    FunctionName: process.env.NEXT_PUBLIC_LAMBDA_FUNCTION_NAME,
     Payload: JSON.stringify(visitorDetails)
   };
   const command = new InvokeCommand(input)
