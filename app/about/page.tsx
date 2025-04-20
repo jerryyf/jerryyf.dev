ee313ed029cfcbff7692b1f99741b2712f008a1c
jerryyf <jerryyf@linsomnia.net>
2025-04-21 05:51:12 +1000

    add AWS dev associate badge

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,8 +12,13 @@ export default function Page() {
       <p className="mb-4">
         {`Graduate: Computer Science - Security Engineering`}
       </p>
-      <div className="my-8">
-        <CredlyBadge badgeId={"9836a4d5-2be7-4b7f-937b-32eb99e839b3"} />
+      <div className="flex gap-4 mb-4">
+        <div className="my-8">
+          <CredlyBadge badgeId={"9836a4d5-2be7-4b7f-937b-32eb99e839b3"} />
+        </div>
+        <div className="my-8">
+          <CredlyBadge badgeId={"0a8c9684-b7f1-4376-88f1-7086876e96cb"} />
+        </div>
       </div>
       <p className="mb-4">
         {`Email: contact [at] jerryyf [dot] dev`}
