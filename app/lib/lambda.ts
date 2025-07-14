2a73a75c048f61e399c9252d59dc40e85d3e7d17
jerryyf <jerry@jerryyf.dev>
2025-07-15 02:12:23 +1000

    feat: kubernetes

--- app/lib/lambda.ts
+++ app/lib/lambda.ts
@@ -4,17 +4,14 @@ import { fromEnv } from '@aws-sdk/credential-providers'
 export async function triggerLambda(visitorDetails:Object) {
 
   // Workaround for Next.js environment variables during build time
-  process.env.AWS_ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID
-  process.env.AWS_SECRET_ACCESS_KEY = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
-  process.env.LAMBDA_FUNCTION_NAME = process.env.NEXT_PUBLIC_LAMBDA_FUNCTION_NAME
 
   const client = new LambdaClient({
     credentials: fromEnv(),
-    region: process.env.NEXT_PUBLIC_AWS_REGION
+    region: process.env.AWS_REGION
   })
 
   const input = {
-    FunctionName: process.env.NEXT_PUBLIC_LAMBDA_FUNCTION_NAME,
+    FunctionName: process.env.LAMBDA_FUNCTION_NAME,
     Payload: JSON.stringify(visitorDetails)
   };
   const command = new InvokeCommand(input)
